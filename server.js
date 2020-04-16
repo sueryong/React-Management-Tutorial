7c39ff4e7c4f538a0f8192b8af98f78a91d089ea
sueryong <csr0604@hanmail.net>
2020-04-16 23:13:43 +0900

    Add Get Customers REST API module

--- server.js
+++ server.js
@@ -3,11 +3,37 @@ const bodyParser = require('body-parser');
 const app =express();
 const port = process.env.PORT || 5000;
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello',(req,res)=>{
-    res.send({message: 'Hello Exdpress!'});
+app.get('/api/customers', (req,res)=>{
+    res.send([
+            {
+              'id': 1,
+              'image': 'https://placeimg.com/64/64/1',
+              'name': '홍길동',
+              'birthday': '961222',
+              'gender': '남자',
+              'job': '대학생'
+            },
+            {
+              'id': 2,
+              'image': 'https://placeimg.com/64/64/2',
+              'name': '채수룡',
+              'birthday': '940604',
+              'gender': '남자',
+              'job': '프로그래머'
+            },
+            {
+              'id': 3,
+              'image': 'https://placeimg.com/64/64/3',
+              'name': '채용비',
+              'birthday': '100630',
+              'gender': '남자',
+              'job': '초딩'
+            }
+          ]);
 });
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
